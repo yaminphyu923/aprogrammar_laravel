22f3a4f8bfdd591bc9326bbeebc2e76da47e2bc0
yaminphyu923 <yaminphyu1237@gmail.com>
2020-03-13 21:43:27 +0630

    upload resources week5

---
+++ resources/views/create.blade.php
@@ -0,0 +1,40 @@
+@extends('layout')
+
+@section("content")
+<div class="container">
+	<h2>Add New Receipe</h2>
+
+	@if ($errors->any())
+	    <div class="alert alert-danger">
+	        <ul>
+	            @foreach ($errors->all() as $error)
+	                <li>{{ $error }}</li>
+	            @endforeach
+	        </ul>
+	    </div>
+	@endif
+
+	<form method="post" action="/receipe">
+		{{ csrf_field()}}
+		<div class="form-group">
+			<label for="name">Receipe Name</label>
+			<input type="text" name="name" class="form-control" id="name" value="{{old('name')}}" required>
+		</div>
+		<div class="form-group">
+			<label for="ingredients">Ingredient</label>
+			<input type="text" name="ingredients" class="form-control" id="ingredients" value="{{old('ingredients')}}" required>
+		</div>
+
+		<div class="form-group">
+			<label for="category">Category</label>
+			<select class="form-control" name="category">
+				@foreach($category as $value)
+					<option value="{{$value->id}}">{{$value->name}}</option>
+				@endforeach
+			</select>
+		</div>
+		<button type="submit" class="btn btn-primary">Submit</button>
+	</form>
+</div>
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+	<div class="row justify-content-center">
+		<div class="col-md-8">
+			<div class="card">
+				<div class="card-header">Dashboard</div>
+
+				<div class="card-body">
+					@if (session('status'))
+					<div class="alert alert-success" role="alert">
+						{{ session('status') }}
+					</div>
+					@endif
+
+					<div class="container">
+						<h2>Home Page</h2>
+						<div>
+							<a href="/receipe/create"><button class="btn btn-success">Create</button></a>
+						</div>
+
+						@foreach($data as $value)
+						<a href="/receipe/{{$value->id}}"><li>{{$value->name}}</li></a>
+						<hr>
+						@endforeach
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+@endsection
